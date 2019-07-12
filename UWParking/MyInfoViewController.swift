860e59a43a341f5ea0b0eb883e5cc8c527e5d2f3
Jiawen-Zhang <zhangjiawen281x@gmail.com>
2019-07-12 10:50:27 +0800

    add Phone, Email, Navi URL

--- UWParking/MyInfoViewController.swift
+++ UWParking/MyInfoViewController.swift
@@ -8,9 +8,38 @@
 
 import Foundation
 import UIKit
+import MapKit
 
 
 class MyInfoViewController: UIViewController{
+    
+    @IBOutlet weak var Phone: UIButton!
+    
+    @IBAction func PhoneTapped(_ sender: Any) {
+        let url:URL?=URL.init(string: "tel:519-888-4567,,,33100")
+        UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+    }
+    
+    
+    @IBOutlet weak var Email: UIButton!
+    
+    @IBAction func EmailTapped(_ sender: Any) {
+        let url:URL?=URL.init(string: "mailto:uparking@uwaterloo.ca")
+        UIApplication.shared.open(url!, options: [:], completionHandler: nil)
+    }
+    
+    @IBOutlet weak var Navi: UIButton!
+    
+    @IBAction func NaviTapped(_ sender: Any) {
+        let latitude = 43.4734361326
+        let longitude = -80.5425853282
+        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        let currentLocation = MKMapItem.forCurrentLocation()
+        let toLocation = MKMapItem(placemark:MKPlacemark(coordinate:location,addressDictionary:nil))
+        toLocation.name = "UW Parking Services"
+        MKMapItem.openMaps(with: [currentLocation,toLocation], launchOptions: [MKLaunchOptionsDirectionsModeKey: MKLaunchOptionsDirectionsModeDriving])
+    }
+    
     
     @IBOutlet weak var Homepage: UIButton!
     
